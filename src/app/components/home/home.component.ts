d5303e16cbe449831e12d6352cd0a6c036308c87
jmbl1685 <jmbl1685@hotmail.com>
2018-11-14 19:35:43 -0500

    FIX: Video background

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,12 +10,18 @@ export class HomeComponent implements OnInit {
   title: any;
   menuOptions: any;
 
+  img:String = "assets/img/wallpaper.jpg"
+
   constructor() {
     this.GenerateTitle();
     this.GenerateOptions();
   }
 
   ngOnInit() { }
+
+  vidEnded() {
+    console.log("video ended");
+  }
 
   GenerateTitle(){
     this.title = "dasdsds"
